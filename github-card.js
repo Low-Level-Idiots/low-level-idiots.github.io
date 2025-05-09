39157d534d03b2a9fe7d4b8b4be2bdc1a848f324
sudotto <codekiwi132@gmail.com>
2025-05-09 14:01:44 -0700

    shit works idk what to say anymore

---
+++ github-card.js
@@ -0,0 +1,19 @@
+function make_card(username){
+	let user_card = document.getElementById(username);
+	let pic = document.createElement("img");
+	let name = document.createElement("b");
+	let url = "https://api.github.com/users/" + username;
+	fetch(url)
+		.then(res => res.json())
+		.then(data => {
+			pic.src = data.avatar_url;
+			console.log(data.avatar_url);
+			name.innerHTML = data.login;
+		});
+	user_card.appendChild(pic);
+	user_card.appendChild(name);
+}
+
+make_card("sudotto");
+make_card("ball15ta");
+make_card("migagula");
