c4bd8f294b598d7ab8fb680ef0471bdd415c59b5
sudotto <codekiwi132@gmail.com>
2025-05-10 09:26:18 -0700

    github cards broke

---
+++ script.js
@@ -0,0 +1,22 @@
+function make_card(username, rolename){
+	let user_card = document.getElementById(username);
+	let pic = document.createElement("img");
+	let name = document.createElement("b");
+	let role = document.createElement("b");
+	let url = "https://api.github.com/users/" + username;
+	fetch(url)
+		.then(res => res.json())
+		.then(data => {
+			pic.src = data.avatar_url;
+			console.log(data.avatar_url);
+			name.innerHTML = data.login;
+			role.innerHTML = rolename;
+		});
+	user_card.appendChild(pic);
+	user_card.appendChild(name);
+	user_card.appendChild(role);
+}
+
+make_card("sudotto", "program");
+make_card("ball15ta", "program");
+make_card("migagula", "program");
