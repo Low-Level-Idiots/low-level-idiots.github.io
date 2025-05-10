55cfaa2a6278a4f8d7a978ecef8deda6786594cb
sudotto <codekiwi132@gmail.com>
2025-05-10 01:33:33 -0700

    roles

--- github-card.js
+++ github-card.js
@@ -10,12 +10,13 @@ function make_card(username, rolename){
 			pic.src = data.avatar_url;
 			console.log(data.avatar_url);
 			name.innerHTML = data.login;
-			role.innerHTML = data.login;
+			role.innerHTML = rolename;
 		});
 	user_card.appendChild(pic);
 	user_card.appendChild(name);
+	user_card.appendChild(role);
 }
 
-make_card("sudotto");
-make_card("ball15ta");
-make_card("migagula");
+make_card("sudotto", "program");
+make_card("ball15ta", "program";
+make_card("migagula", "program";
